08cee4ad818ebc2eea1a1fc85f035261dea7147e
Laura Wolfart <laurawolfart@gmail.com>
2022-01-05 18:32:13 -0500

    adding links

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
+//Interpersonal Skills
 new CircleType(document.getElementById('arc1')).radius(260);
-new CircleType(document.getElementById('arc2')).radius(260);
-new CircleType(document.getElementById('arc3')).radius(260);
-new CircleType(document.getElementById('arc4')).radius(260);
-new CircleType(document.getElementById('arc5')).radius(260).dir(-1);
-new CircleType(document.getElementById('arc6')).radius(260).dir(-1);
-new CircleType(document.getElementById('arc7')).radius(260).dir(-1);
-
 $('.outer-1').css({'transform':'rotate(284deg)'})
-$('.outer-2').css({'transform':'rotate(335deg)'})
-$('.outer-3').css({'transform':'rotate(24deg)'})
-$('.outer-4').css({'transform':'rotate(76deg)'})
-$('.outer-5').css({'transform':'rotate(309deg)'})
-$('.outer-7').css({'transform':'rotate(49deg)'})
-
-$('.outer-1').hover(function() {
+$('a.outer-1').hover(function() {
     $(this).css({
         'transform':'scale(1.5) rotate(284deg)'
     })
@@ -23,7 +11,52 @@ $('.outer-1').hover(function() {
     })
 })
 
-$('.outer-2').hover(function() {
+//Interpersonal Skills -> Action Accelerators
+new CircleType(document.getElementById('inner-arc0')).radius(150);
+new CircleType(document.getElementById('inner-arc0-m')).radius(150);
+$('.inner-01').css({'transform':'rotate(283deg)'})
+$('a.inner-01').hover(function() {
+    $(this).css({
+        'transform':'scale(1.2) rotate(283deg)'
+    })
+}, function() {
+    $(this).css({
+        'transform':'scale(1) rotate(283deg)'
+    })
+})
+
+//Interpersonal Skills -> Online Courses
+new CircleType(document.getElementById('inner-arc02')).radius(120);
+new CircleType(document.getElementById('inner-arc02-m')).radius(120);
+$('.inner-02').css({'transform':'rotate(282deg)'})
+$('a.inner-02').hover(function() {
+    $(this).css({
+        'transform':'scale(1.2) rotate(282deg)'
+    })
+}, function() {
+    $(this).css({
+        'transform':'scale(1) rotate(282deg)'
+    })
+})
+
+//Interpersonal Skills -> Resources
+new CircleType(document.getElementById('inner-arc03')).radius(70);
+new CircleType(document.getElementById('inner-arc03-m')).radius(70);
+$('.inner-03').css({'transform':'rotate(282deg)'})
+$('a.inner-03').hover(function() {
+    $(this).css({
+        'transform':'scale(1.2) rotate(282deg)'
+    })
+}, function() {
+    $(this).css({
+        'transform':'scale(1) rotate(282deg)'
+    })
+})
+
+//M&L
+new CircleType(document.getElementById('arc2')).radius(260);
+$('.outer-2').css({'transform':'rotate(335deg)'})
+$('a.outer-2').hover(function() {
     $(this).css({
         'transform':'scale(1.5) rotate(335deg)'
     })
@@ -33,7 +66,52 @@ $('.outer-2').hover(function() {
     })
 })
 
-$('.outer-3').hover(function() {
+// M&L -> Action Accelerators
+new CircleType(document.getElementById('inner-arc1')).radius(150);
+new CircleType(document.getElementById('inner-arc1-m')).radius(150);
+$('.inner-1').css({'transform':'rotate(334deg)'})
+$('a.inner-1').hover(function() {
+    $(this).css({
+        'transform':'scale(1.2) rotate(334deg)'
+    })
+}, function() {
+    $(this).css({
+        'transform':'scale(1) rotate(334deg)'
+    })
+})
+
+// M&L -> Online Courses
+new CircleType(document.getElementById('inner-arc2')).radius(120);
+new CircleType(document.getElementById('inner-arc2-m')).radius(120);
+$('.inner-2').css({'transform':'rotate(334deg)'})
+$('a.inner-2').hover(function() {
+    $(this).css({
+        'transform':'scale(1.2) rotate(334deg)'
+    })
+}, function() {
+    $(this).css({
+        'transform':'scale(1) rotate(334deg)'
+    })
+})
+
+// M&L -> Resources
+new CircleType(document.getElementById('inner-arc3')).radius(70);
+new CircleType(document.getElementById('inner-arc3-m')).radius(70);
+$('.inner-3').css({'transform':'rotate(334deg)'})
+$('a.inner-3').hover(function() {
+    $(this).css({
+        'transform':'scale(1.2) rotate(334deg)'
+    })
+}, function() {
+    $(this).css({
+        'transform':'scale(1) rotate(334deg)'
+    })
+})
+
+//Induction
+new CircleType(document.getElementById('arc3')).radius(260);
+$('.outer-3').css({'transform':'rotate(24deg)'})
+$('a.outer-3').hover(function() {
     $(this).css({
         'transform':'scale(1.5) rotate(24deg)'
     })
@@ -43,7 +121,52 @@ $('.outer-3').hover(function() {
     })
 })
 
-$('.outer-4').hover(function() {
+//Induction -> Action Accelerators
+new CircleType(document.getElementById('inner-arc11')).radius(150);
+new CircleType(document.getElementById('inner-arc11-m')).radius(150);
+$('.inner-11').css({'transform':'rotate(385deg)'})
+$('a.inner-11').hover(function() {
+    $(this).css({
+        'transform':'scale(1.2) rotate(385deg)'
+    })
+}, function() {
+    $(this).css({
+        'transform':'scale(1) rotate(385deg)'
+    })
+})
+
+//Induction -> Online Courses
+new CircleType(document.getElementById('inner-arc12')).radius(120);
+new CircleType(document.getElementById('inner-arc12-m')).radius(120);
+$('.inner-12').css({'transform':'rotate(385deg)'})
+$('a.inner-12').hover(function() {
+    $(this).css({
+        'transform':'scale(1.2) rotate(385deg)'
+    })
+}, function() {
+    $(this).css({
+        'transform':'scale(1) rotate(385deg)'
+    })
+})
+
+//Induction -> Resources
+new CircleType(document.getElementById('inner-arc13')).radius(70);
+new CircleType(document.getElementById('inner-arc13-m')).radius(70);
+$('.inner-13').css({'transform':'rotate(385deg)'})
+$('a.inner-13').hover(function() {
+    $(this).css({
+        'transform':'scale(1.2) rotate(385deg)'
+    })
+}, function() {
+    $(this).css({
+        'transform':'scale(1) rotate(385deg)'
+    })
+})
+
+//Culture
+new CircleType(document.getElementById('arc4')).radius(260);
+$('.outer-4').css({'transform':'rotate(76deg)'})
+$('a.outer-4').hover(function() {
     $(this).css({
         'transform':'scale(1.5) rotate(76deg)'
     })
@@ -53,7 +176,52 @@ $('.outer-4').hover(function() {
     })
 })
 
-$('.outer-5').hover(function() {
+//Culture -> Action Accelerators
+new CircleType(document.getElementById('inner-arc21')).radius(150);
+new CircleType(document.getElementById('inner-arc21-m')).radius(150);
+$('.inner-21').css({'transform':'rotate(435deg)'})
+$('a.inner-21').hover(function() {
+    $(this).css({
+        'transform':'scale(1.2) rotate(435deg)'
+    })
+}, function() {
+    $(this).css({
+        'transform':'scale(1) rotate(435deg)'
+    })
+})
+
+//Culture -> Online Courses
+new CircleType(document.getElementById('inner-arc22')).radius(120);
+new CircleType(document.getElementById('inner-arc22-m')).radius(120);
+$('.inner-22').css({'transform':'rotate(435deg)'})
+$('a.inner-22').hover(function() {
+    $(this).css({
+        'transform':'scale(1.2) rotate(435deg)'
+    })
+}, function() {
+    $(this).css({
+        'transform':'scale(1) rotate(435deg)'
+    })
+})
+
+//Culture -> Resources
+new CircleType(document.getElementById('inner-arc23')).radius(70);
+new CircleType(document.getElementById('inner-arc23-m')).radius(70);
+$('.inner-23').css({'transform':'rotate(435deg)'})
+$('a.inner-23').hover(function() {
+    $(this).css({
+        'transform':'scale(1.2) rotate(435deg)'
+    })
+}, function() {
+    $(this).css({
+        'transform':'scale(1) rotate(435deg)'
+    })
+})
+
+//Luxury
+new CircleType(document.getElementById('arc5')).radius(260).dir(-1);
+$('.outer-5').css({'transform':'rotate(309deg)'})
+$('a.outer-5').hover(function() {
     $(this).css({
         'transform':'scale(1.5) rotate(309deg)'
     })
@@ -62,8 +230,67 @@ $('.outer-5').hover(function() {
         'transform':'scale(1) rotate(309deg)'
     })
 })
+//Luxury -> Action Accelerators
+new CircleType(document.getElementById('inner-arc33')).radius(150).dir(-1);
+new CircleType(document.getElementById('inner-arc33-m')).radius(150).dir(-1);
+$('.inner-33').css({'transform':'rotate(308deg)'})
+$('a.inner-33').hover(function() {
+    $(this).css({
+        'transform':'scale(1.2) rotate(308deg)'
+    })
+}, function() {
+    $(this).css({
+        'transform':'scale(1) rotate(308deg)'
+    })
+})
 
-$('.outer-7').hover(function() {
+//Luxury -> Online Courses
+new CircleType(document.getElementById('inner-arc32')).radius(120).dir(-1);
+new CircleType(document.getElementById('inner-arc32-m')).radius(120).dir(-1);
+$('.inner-32').css({'transform':'rotate(308deg)'})
+$('a.inner-32').hover(function() {
+    $(this).css({
+        'transform':'scale(1.2) rotate(308deg)'
+    })
+}, function() {
+    $(this).css({
+        'transform':'scale(1) rotate(308deg)'
+    })
+})
+
+//Luxury -> Resources
+new CircleType(document.getElementById('inner-arc31')).radius(70).dir(-1);
+new CircleType(document.getElementById('inner-arc31-m')).radius(70).dir(-1);
+$('.inner-31').css({'transform':'rotate(308deg)'})
+$('a.inner-31').hover(function() {
+    $(this).css({
+        'transform':'scale(1.2) rotate(308deg)'
+    })
+}, function() {
+    $(this).css({
+        'transform':'scale(1) rotate(308deg)'
+    })
+})
+
+//D&I
+new CircleType(document.getElementById('arc6')).radius(260).dir(-1);
+
+//D&I -> Action Accelerators
+new CircleType(document.getElementById('inner-arc6')).radius(150).dir(-1);
+new CircleType(document.getElementById('inner-arc6-m')).radius(150).dir(-1);
+
+//D&I -> Online Courses
+new CircleType(document.getElementById('inner-arc5')).radius(120).dir(-1);
+new CircleType(document.getElementById('inner-arc5-m')).radius(120).dir(-1);
+
+//D&I -> Resources
+new CircleType(document.getElementById('inner-arc4')).radius(70).dir(-1);
+new CircleType(document.getElementById('inner-arc4-m')).radius(70).dir(-1);
+
+//Academy
+new CircleType(document.getElementById('arc7')).radius(260).dir(-1);
+$('.outer-7').css({'transform':'rotate(49deg)'})
+$('a.outer-7').hover(function() {
     $(this).css({
         'transform':'scale(1.5) rotate(49deg)'
     })
@@ -73,9 +300,44 @@ $('.outer-7').hover(function() {
     })
 })
 
-new CircleType(document.getElementById('inner-arc1')).radius(150);
-new CircleType(document.getElementById('inner-arc2')).radius(120);
-new CircleType(document.getElementById('inner-arc3')).radius(70);
-new CircleType(document.getElementById('inner-arc4')).radius(70).dir(-1);
-new CircleType(document.getElementById('inner-arc5')).radius(120).dir(-1);
-new CircleType(document.getElementById('inner-arc6')).radius(150).dir(-1);
+//Academy -> Action Accelerators
+new CircleType(document.getElementById('inner-arc43')).radius(150).dir(-1);
+new CircleType(document.getElementById('inner-arc43-m')).radius(150).dir(-1);
+$('.inner-43').css({'transform':'rotate(408deg)'})
+$('a.inner-43').hover(function() {
+    $(this).css({
+        'transform':'scale(1.2) rotate(408deg)'
+    })
+}, function() {
+    $(this).css({
+        'transform':'scale(1) rotate(408deg)'
+    })
+})
+
+//Academy -> Online Courses
+new CircleType(document.getElementById('inner-arc42')).radius(120).dir(-1);
+new CircleType(document.getElementById('inner-arc42-m')).radius(120).dir(-1);
+$('.inner-42').css({'transform':'rotate(408deg)'})
+$('a.inner-42').hover(function() {
+    $(this).css({
+        'transform':'scale(1.2) rotate(408deg)'
+    })
+}, function() {
+    $(this).css({
+        'transform':'scale(1) rotate(408deg)'
+    })
+})
+
+//Academy -> Resources
+new CircleType(document.getElementById('inner-arc41')).radius(70).dir(-1);
+new CircleType(document.getElementById('inner-arc41-m')).radius(70).dir(-1);
+$('.inner-41').css({'transform':'rotate(408deg)'})
+$('a.inner-41').hover(function() {
+    $(this).css({
+        'transform':'scale(1.2) rotate(408deg)'
+    })
+}, function() {
+    $(this).css({
+        'transform':'scale(1) rotate(408deg)'
+    })
+})
